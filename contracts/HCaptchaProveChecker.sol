72e5824255d318703532eedd1a34d381c83b9004
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-25 18:03:08 +0200

    change: check and add proof method name

--- contracts/HCaptchaProveChecker.sol
+++ contracts/HCaptchaProveChecker.sol
@@ -2,22 +2,34 @@
 pragma solidity ^0.8.18;
 
 import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import {IProveChecker} from "./IProveChecker.sol";
 
-contract HCaptchaProveChecker is Ownable {
+contract HCaptchaProveChecker is Ownable, IProveChecker {
     address private _allowedAddAddress;
     mapping(address => string) private _confirmedCodes;
 
-    constructor(address allowedAddAddress) {
+    constructor(address allowedAddAddress) Ownable(msg.sender) {
         _allowedAddAddress = allowedAddAddress;
     }
 
-    function addConfirmedCode(address confirmedPerson, string memory code) external {
-        require(msg.sender == _allowedAddAddress, "Only authorized address can add confirmed code");
+    function addProof(address confirmedPerson, string memory code) external {
+        require(
+            msg.sender == _allowedAddAddress,
+            "Only authorized address can add confirmed code"
+        );
+        require(confirmedPerson != address(0), "Address cannot be empty");
         require(bytes(code).length > 0, "Code cannot be empty");
-        _confirmedCodes[code] = true;
+        _confirmedCodes[confirmedPerson] = code;
     }
 
-    function checkConfirmedCode(address attestedPerson, string memory code) external returns (bool) {
-        return _confirmedCodes[attestedPerson] == code;
+    function checkProof(
+        address attestedPerson,
+        string memory code
+    ) external view returns (bool) {
+        require(attestedPerson != address(0), "Address cannot be empty");
+        require(bytes(code).length > 0, "Code cannot be empty");
+        return
+            keccak256(abi.encodePacked(_confirmedCodes[attestedPerson])) ==
+            keccak256(abi.encodePacked(code));
     }
 }
