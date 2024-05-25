32ac566bdc9631a6f7d976f90026ddee185ee9b0
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-25 18:54:21 +0200

    change: deployment output format

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -12,15 +12,16 @@ contract DeployScript is Script {
         uint256 deployerPrivateKey = vm.envUint("SEPOLIA_PRIVATE_KEY");
 
         vm.startBroadcast(deployerPrivateKey);
+        console.log("Copy the following values to your .env file!");
 
         HCaptchaProveChecker proveChecker = new HCaptchaProveChecker(vm.addr(deployerPrivateKey));
-        console.log("proveChecker deployed to:", address(proveChecker));
+        console.log("NEXT_PUBLIC_PROVE_CHECKER_SMART_CONTRACT_ADDRESS=", address(proveChecker));
 
         Report report = new Report(address(proveChecker));
-        console.log("report deployed to:", address(report));
+        console.log("NEXT_PUBLIC_REPORT_SMART_CONTRACT_ADDRESS=", address(report));
 
         ReportFaucet reportFaucet = new ReportFaucet(vm.addr(deployerPrivateKey));
-        console.log("reportFaucet deployed to:", address(reportFaucet));
+        console.log("NEXT_PUBLIC_FAUCET_SMART_CONTRACT_ADDRESS=", address(reportFaucet));
 
         vm.stopBroadcast();
     }
