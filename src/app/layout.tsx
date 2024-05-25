335ac62d47408a9c09f934477509968a5cfa1f20
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-25 02:34:40 +0200

    add: radix ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/app/providers";
+import { Theme } from "@radix-ui/themes";
+
+import '@radix-ui/themes/styles.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Theme>
+          <Providers>
+            {children}
+          </Providers>
+        </Theme>
       </body>
     </html>
   );
