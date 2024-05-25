c1804295044db02c396fbd28839c5167af927262
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-25 15:30:13 +0200

    add: hcaptcha code storage contract

---
+++ contracts/HCaptchaProveChecker.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract HCaptchaProveChecker is Ownable {
+    address private _allowedAddAddress;
+    mapping(address => string) private _confirmedCodes;
+
+    constructor(address _allowedAddAddress) {
+        _allowedAddAddress = _allowedAddAddress;
+    }
+
+    function addConfirmedCode(address confirmedPerson, string memory code) external {
+        require(msg.sender == _allowedAddAddress, "Only authorized address can add confirmed code");
+        require(bytes(code).length > 0, "Code cannot be empty");
+        _confirmedCodes[code] = true;
+    }
+
+    function checkConfirmedCode(address attestedPerson, string memory code) external returns (bool) {
+        return _confirmedCodes[attestedPerson] == code;
+    }
+}
