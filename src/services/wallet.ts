06a2a095801aa8ea56c4ccd307e47c2134c3f52a
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-25 19:02:07 +0200

    chore: run prettier

--- src/services/wallet.ts
+++ src/services/wallet.ts
@@ -1,3 +1,8 @@
 import ethers from "ethers";
-const provider = new ethers.providers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
-export const serverWallet = new ethers.Wallet(process.env.SEPOLIA_PRIVATE_KEY!, provider);
+const provider = new ethers.providers.JsonRpcProvider(
+  process.env.SEPOLIA_RPC_URL,
+);
+export const serverWallet = new ethers.Wallet(
+  process.env.SEPOLIA_PRIVATE_KEY!,
+  provider,
+);
