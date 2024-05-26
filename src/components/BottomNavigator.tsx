699888709bd5ee842018ffcfb6d09936b119a962
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-26 10:51:23 +0200

    save: work

--- src/components/BottomNavigator.tsx
+++ src/components/BottomNavigator.tsx
@@ -1,21 +1,36 @@
 import Link from "next/link";
 import React from "react";
+import { Button, Text } from "@radix-ui/themes";
 
 const BottomNavigator: React.FC = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 text-white flex justify-evenly">
-      <Link
-        className="flex flex-col items-center bg-navBtn py-4 px-8 my-5 w-2/5 rounded-md"
-        href="/"
+    <div className="bg-background fixed py-5 bottom-0 left-0 right-0 text-white flex items-center justify-center gap-4">
+      <Button
+        size={"3"}
+        type={"button"}
+        variant={"surface"}
+        className={"w-2/5"}
+        asChild
       >
-        <span className="font-display">Report</span>
-      </Link>
-      <Link
-        className="flex flex-col items-center bg-navBtn py-4 px-8 my-5 w-2/5 rounded-md"
-        href="/feed"
+        <Link href="/">
+          <Text size={"1"} className="font-display">
+            Report
+          </Text>
+        </Link>
+      </Button>
+      <Button
+        size={"3"}
+        type={"button"}
+        variant={"surface"}
+        className={"w-2/5"}
+        asChild
       >
-        <span className="font-display">Feed</span>
-      </Link>
+        <Link href="/feed">
+          <Text size={"1"} className="font-display">
+            Feed
+          </Text>
+        </Link>
+      </Button>
     </div>
   );
 };
