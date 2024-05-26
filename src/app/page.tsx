d2f5b57dfe1cafe64bd716871a747ba0cbbaabf3
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-26 08:32:54 +0200

    add: request faucet when submitting report and submitting report itself

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,30 @@
 
 import { Button, Text, TextArea, TextField } from "@radix-ui/themes";
 import WalletConnectButton from "@/components/WalletConnectButton";
-import { useActiveWallet, useConnectedWallets } from "thirdweb/react";
+import { useActiveWallet, useSendAndConfirmTransaction, useSendTransaction } from "thirdweb/react";
 import classNames from "classnames";
 import * as Form from "@radix-ui/react-form";
 import HCaptcha from "@hcaptcha/react-hcaptcha";
 import toast from "react-hot-toast";
+import { FormEvent, useRef, useState } from "react";
+import { prepareContractCall, getContract } from "thirdweb";
+import { client } from "@/services/ThirdWeb";
+import { sepolia } from "thirdweb/chains";
+import { requestFaucet } from "@/services/RequestFaucet";
+
+const contract = getContract({
+  client,
+  chain: sepolia,
+  address: process.env.NEXT_PUBLIC_REPORT_SMART_CONTRACT_ADDRESS!,
+});
 
 const Home = () => {
   const wallet = useActiveWallet();
+  const titleRef = useRef<HTMLInputElement>(null);
+  const descRef = useRef<HTMLTextAreaElement>(null);
+  const captchaRef = useRef<HCaptcha>(null);
+  const [captchaSubmitted, setCaptchaSubmitted] = useState(false);
+  const { mutate: sendTransaction, isPending } = useSendAndConfirmTransaction();
 
   const submitCaptchaToContract = (captchaCode: string) => {
     if (wallet && wallet.getAccount()) {
@@ -22,7 +38,8 @@ const Home = () => {
         fetch("/api/verify", {
           method: "POST",
           body: formData,
-        }).then((res) => res.json()),
+        }).then((res) => res.json())
+          .then((res) => setCaptchaSubmitted(true)),
         {
           loading: "Submitting captcha to chain",
           success: "Successfully submitted captcha",
@@ -32,10 +49,46 @@ const Home = () => {
     }
   };
 
+  const submitFunction = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!wallet || !titleRef.current || !descRef.current || !captchaRef.current) {
+      console.error("Title or desc ref is null")
+    } else {
+      await toast.promise(requestFaucet(wallet.getAccount()?.address!), {
+        loading: "Requesting fund for your wallet",
+        success: "Successfully funded your wallet",
+        error: (err) => `Error funding your wallet: ${err.message}`,
+      })
+
+      const title = titleRef.current.value;
+      const desc = descRef.current.value;
+      const captcha = captchaRef.current.getResponse();
+
+      const transaction = prepareContractCall({
+        contract,
+        method: "function createReport(string memory title, string memory description, string memory proof) public returns (uint256)",
+        params: [title, desc, captcha],
+      });
+      const transactionPromise = new Promise<void>((resolve, reject) => {
+        sendTransaction(transaction, {
+          onSuccess: () => resolve(), // Resolve on success
+          onError: (error) => reject(error), // Reject with the error
+        });
+      });
+
+      toast.promise(transactionPromise, {
+        loading: "Submitting report to the blockchain...",
+        success: "Report successfully submitted!",
+        error: (err) => `Error submitting report: ${err.message}`,
+      });
+    }
+  }
+
   return (
     <main
       className={
-        "flex flex-col px-10 h-max justify-center items-center gap-12 self-center"
+        "flex flex-col px-10 h-max justify-center items-center gap-12 self-center pb-24"
       }
     >
       <Text
@@ -51,7 +104,7 @@ const Home = () => {
         <WalletConnectButton />
       </div>
       {wallet && (
-        <Form.Root className={"w-full flex flex-col gap-4"}>
+        <Form.Root className={"w-full flex flex-col gap-4"} onSubmit={event => submitFunction(event)}>
           <Form.Field name={"title"} className={"flex flex-col gap-2"}>
             <div className={"flex flex-col"}>
               <Form.Label className="font-display">Title</Form.Label>
@@ -59,7 +112,7 @@ const Home = () => {
                 match="valueMissing"
                 className={"text-red-600 font-display"}
               >
-                Please enter a title
+                <Text size={"1"}>Please enter a title</Text>
               </Form.Message>
             </div>
             <Form.Control asChild>
@@ -68,6 +121,7 @@ const Home = () => {
                 size={"3"}
                 required={true}
                 className="font-display"
+                ref={titleRef}
               ></TextField.Root>
             </Form.Control>
           </Form.Field>
@@ -78,13 +132,13 @@ const Home = () => {
                 match="valueMissing"
                 className={"text-red-600 font-display"}
               >
-                Please enter a description
+                <Text size={"1"}>Please enter a description</Text>
               </Form.Message>
               <Form.Message
                 match="tooShort"
                 className={"text-red-600 font-display"}
               >
-                Description is too short
+                <Text size={"1"}>Description is too short</Text>
               </Form.Message>
             </div>
             <Form.Control asChild>
@@ -94,6 +148,7 @@ const Home = () => {
                 minLength={20}
                 required={true}
                 className="font-display h-40"
+                ref={descRef}
               ></TextArea>
             </Form.Control>
           </Form.Field>
@@ -101,10 +156,11 @@ const Home = () => {
             <HCaptcha
               sitekey={"a4da0cb6-48fc-4b96-94e0-6eb2397e04e2"}
               onVerify={(token) => submitCaptchaToContract(token)}
+              ref={captchaRef}
             />
           </Form.Field>
-          <Form.Submit>
-            <Button className={"w-full font-display py-5"}>Submit</Button>
+          <Form.Submit asChild>
+            <Button className={"w-full font-display py-5"} disabled={!captchaSubmitted}>Submit</Button>
           </Form.Submit>
         </Form.Root>
       )}
---
+++ src/services/ThirdWeb.ts
@@ -0,0 +1,3 @@
+import { createThirdwebClient } from "thirdweb";
+
+export const client = createThirdwebClient({ clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID! });
